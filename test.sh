35427fd7c9886077854b766c8cca531ce9853d39
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2025-03-15 14:09:14 -0500

    Print test stdout while writing to file

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ DATASET=${1:-diabetes}
 CORRECT_ICON="\U2705"
 WRONG_ICON="\U274C"
 
-./genetic_benchmark "$DATASET" > "output/$DATASET.txt"
+./genetic_benchmark "$DATASET" | tee "output/$DATASET.txt"
 
 # Compare all lines except the last one, which has the runtime
 if cmp --silent <(head -n -1 "output/$DATASET.txt") <(head -n -1 "output/${DATASET}_correct.txt"); then
